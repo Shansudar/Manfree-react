fc4bb22683480c9b0f964812358901686a2f050c
mukil <mukilathuruvan5241@gmail.com>
2022-11-15 19:05:13 +0530

    creating first component

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header-div">
+      <h2>this is header</h2>
+    </div>
+  );
+};
+
+export const Footer = () => {
+  return (
+    <div>
+      <h2>this is Footer</h2>
+    </div>
+  );
+};
+export default Header;
+
+
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Header from './components/Header';
 import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const node = document.getElementById("root");
+const root = ReactDOM.createRoot(node);
 root.render(
   <React.StrictMode>
     <App />
