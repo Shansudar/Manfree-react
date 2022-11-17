3aa2323cb10b12f3e0f52113f9f39639b1cc8b91
mukil <mukilathuruvan5241@gmail.com>
2022-11-17 19:09:17 +0530

    rendering elements based on condition

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-import { Array } from "./components/Array";
+import { Array, User } from "./components/Array";
 function App() {
   return (
     <div className="App">
       <Array />
+      <User />
     </div>
   );
 }
