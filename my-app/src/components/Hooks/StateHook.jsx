666deb65317abec23e76ac6e482cd5e2bc43d422
mukil <mukilathuruvan5241@gmail.com>
2022-11-22 21:28:53 +0530

    using anonymous function to set state

--- my-app/src/components/Hooks/StateHook.jsx
+++ my-app/src/components/Hooks/StateHook.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 
 const StateHook = () => {
+  const start = 1 0;
   console.log("component is rendering");
-  const [count, setCount] = useState(0);
-  const handleInr = () => {
-    setCount(count + 1);
-  };
+  const [count, setCount] = useState(start);
+  // const handleInr = () => {
+  //   setCount(count + 1);
+  // };
   return (
     <div>
       <h2>{count}</h2>
-      <button onClick={handleInr}>Click</button>
+      <button onClick={() => setCount(count + 1)}> Click</button>
+      <button onClick={() => setCount(start)}>Reset</button>
     </div>
   );
 };
