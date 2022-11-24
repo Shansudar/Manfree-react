024799dfc93d3bf11c11109d8606dfb67c6cb5c1
mukil <mukilathuruvan5241@gmail.com>
2022-11-24 19:20:24 +0530

    handling input change event

--- my-app/src/components/Hooks/Mutation.jsx
+++ my-app/src/components/Hooks/Mutation.jsx
@@ -2,17 +2,22 @@ import React, { useState } from "react";
 
 const Mutation = () => {
   const [names, setNames] = useState(["abi", "boobalan", "sudharsan"]);
+  const [value, setValue] = useState("");
   const handleClick = () => {
     // names.push("manfree");
-    setNames([...names, "manfree"]);
+    setNames([...names, value]);
     console.log(names);
+    setValue("");
+  };
+  const handleChange = (e) => {
+    setValue(e.target.value);
   };
   return (
     <div>
       {names.map((item, index) => (
         <li key={index}>{item}</li>
       ))}
-
+      <input type="text" value={value} onChange={handleChange} />
       <button onClick={handleClick}>Click</button>
     </div>
   );
