2ba92bfd8d12132da899d015d12d8997aadd4209
mukil <mukilathuruvan5241@gmail.com>
2022-11-24 19:30:27 +0530

    finding user present in array

--- my-app/src/components/Hooks/Mutation.jsx
+++ my-app/src/components/Hooks/Mutation.jsx
@@ -3,11 +3,18 @@ import React, { useState } from "react";
 const Mutation = () => {
   const [names, setNames] = useState(["abi", "boobalan", "sudharsan"]);
   const [value, setValue] = useState("");
+  const [show, setShow] = useState(false);
   const handleClick = () => {
-    // names.push("manfree");
-    setNames([...names, value]);
-    console.log(names);
-    setValue("");
+    const isPresent = names.find((item) => item === value);
+    if (isPresent) {
+        setShow(true);
+        setValue("")
+    }
+    else {
+      setNames([...names, value]);
+      console.log(names);
+      setValue("");
+    }
   };
   const handleChange = (e) => {
     setValue(e.target.value);
@@ -19,6 +26,7 @@ const Mutation = () => {
       ))}
       <input type="text" value={value} onChange={handleChange} />
       <button onClick={handleClick}>Click</button>
+      <div>{show && <h4>userName already taken</h4>}</div>
     </div>
   );
 };
