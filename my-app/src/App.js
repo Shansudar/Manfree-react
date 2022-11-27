a7a28c7e2bc9994a71c06c79f8db8e8750d7ea2f
mukil <mukilathuruvan5241@gmail.com>
2022-11-27 12:35:11 +0530

    mutation explained

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { TypeOne, TypeThree, TypeTwo } from "./components/Conditional";
-import Intro from "./components/Hooks/Intro";
-import StateHook from "./components/Hooks/StateHook";
-import LifeCycle from "./components/Hooks/LifeCycle";
-import { useState } from "react";
-import Users from "./components/Hooks/Users";
-import Mutation from "./components/Hooks/Mutation";
+import Example from "./components/Example";
 function App() {
-  const [on, setOn] = useState(true);
   return (
     <div className="App">
-     <Mutation/>
+     <Example/>
     </div>
   );
 }
---
+++ my-app/src/components/Example.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const initialState = {
+  name: "ragnar",
+  role: "king",
+};
+
+const Example = () => {
+  const [person, setPerson] = useState(initialState);
+  const keys = Object.keys(person);
+  const handleClick = () => {
+    const another = { ...person, age: 21 };
+    setPerson(another);
+    console.log(person);
+  };
+  return (
+    <div>
+      <div>
+        <ul>
+          {keys.map((item, index) => (
+            <li key={index}>
+              {item}-{person[item]}
+            </li>
+          ))}
+        </ul>
+        <button onClick={handleClick}>Click</button>
+      </div>
+    </div>
+  );
+};
+
+export default Example;
