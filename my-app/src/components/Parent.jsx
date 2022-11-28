9d3b4569bcb9004558aaa3988b01fd7c3b471654
mukil <mukilathuruvan5241@gmail.com>
2022-11-28 18:17:40 +0530

    handling mutiple input changes

--- my-app/src/components/Parent.jsx
+++ my-app/src/components/Parent.jsx
@@ -1,22 +1,48 @@
 import React, { useState } from "react";
 import john from "./one.jpg";
+const person = { fname: "", age: 0, course: "" };
 const Parent = () => {
-  const [name, setName] = useState("");
-  const handleChange = (e) => {
-    let value = e.target.value;
-    setName(value);
-  };
+  const [bot, setBot] = useState(person);
+  // const handleChange = (e) => {
+  //   let value = e.target.value;
+  //   setBot(value);
+  // };
   return (
     <div>
       <form action="">
         <label htmlFor="">name</label>
-        <input type="text" /> <br />
+
+        <input
+          type="text"
+          value={bot.fname}
+          onChange={(e) => setBot({ ...bot, fname: e.target.value })}
+        />
+        <br />
+
         <label htmlFor="">age</label>
-        <input type="text" />
+
+        <input
+          type="number"
+          value={bot.age}
+          onChange={(e) => setBot({ ...bot, age: parseInt(e.target.value) })}
+        />
+
         <br />
         <label htmlFor="">course</label>
-        <input type="text" />
+
+        <input
+          type="text"
+          value={bot.course}
+          onChange={(e) => setBot({ ...bot, course: e.target.value })}
+        />
+        <br />
+        <input type="submit" />
       </form>
+      <div>
+        <h2>
+          {bot.fname}- {bot.age} -{bot.course}
+        </h2>
+      </div>
     </div>
   );
 };
